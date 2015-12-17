da7031053fb8064c0182f3eee6ee08c27689fcdc
Justin Tirrell <jjttjj@gmail.com>
2015-12-17 15:00:25 -0500

    clean up project.clj, add core.async dep, update deps

--- project.clj
+++ project.clj
@@ -1,11 +1,8 @@
 (defproject ib-re-actor "0.1.1-SNAPSHOT"
   :description "Clojure friendly wrapper for InteractiveBrokers java API"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-time "0.4.4"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-time "0.11.0"]
                  [com.ib/jtsclient "9.68"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [clj-logging-config "1.9.10"]]
-  :repositories {"stuart" "http://stuartsierra.com/maven2"}
-  :plugins [[lein-midje "2.0.1"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]
-                                  [com.stuartsierra/lazytest "1.2.3"]]}})
+                 [clj-logging-config "1.9.10"]
+                 [org.clojure/core.async "0.2.374"]])

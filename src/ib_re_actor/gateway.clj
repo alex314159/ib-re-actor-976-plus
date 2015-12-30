7f9d1ddc2830787dff48d231543fd81e0381134b
Justin Tirrell <jjttjj@gmail.com>
2015-12-30 14:01:29 -0500

    change (connect) to return map containing ecs and resp-chan

--- src/ib_re_actor/gateway.clj
+++ src/ib_re_actor/gateway.clj
@@ -427,18 +427,18 @@
 
      if no tick list is specified, a single snapshot of market data will come back
      and have the market data subscription will be immediately canceled."
-  ([conn contract ticker-id tick-list snapshot?]
-   (.reqMktData conn ticker-id
+  ([{:keys [ecs]} contract ticker-id tick-list snapshot?]
+   (.reqMktData ecs ticker-id
                 (map-> com.ib.client.Contract contract)
                 (translate :to-ib :tick-list tick-list)
                 snapshot?))
-  ([conn contract]
+  ([{:keys [ecs]} contract]
    (let [ticker-id (swap! last-ticker-id inc)]
-     (request-market-data conn contract ticker-id "" false)
+     (request-market-data ecs contract ticker-id "" false)
      ticker-id)))
 
-(defn cancel-market-data [conn ticker-id]
-  (.cancelMktData conn ticker-id))
+(defn cancel-market-data [{:keys [ecs]} ticker-id]
+  (.cancelMktData ecs ticker-id))
 
 (defn request-historical-data
   "Start receiving historical price bars stretching back <duration> <duration-unit>s back,
@@ -451,11 +451,11 @@
    what-to-show should be one of :trades, :midpoint, :bid, :ask, :bid-ask,
    :historical-volatility, :option-implied-volatility, :option-volume,
    or :option-open-interest."
-  ([conn id contract end duration duration-unit bar-size bar-size-unit
+  ([{:keys [ecs]} id contract end duration duration-unit bar-size bar-size-unit
     what-to-show use-regular-trading-hours?]
    (let [[acceptable-duration acceptable-duration-unit]
          (translate :to-ib :acceptable-duration [duration duration-unit])]
-     (.reqHistoricalData conn
+     (.reqHistoricalData ecs
                          id
                          (map-> com.ib.client.Contract contract)
                          (translate :to-ib :date-time end)
@@ -474,8 +474,8 @@
 
 (defn request-real-time-bars
   "Start receiving real time bar results."
-  [conn id contract what-to-show use-regular-trading-hours?]
-  (.reqRealTimeBars conn
+  [{:keys [ecs]} id contract what-to-show use-regular-trading-hours?]
+  (.reqRealTimeBars ecs
                     id
                     (map-> com.ib.client.Contract contract)
                     5
@@ -484,83 +484,83 @@
 
 (defn cancel-real-time-bars
   "Call this function to stop receiving real time bars for the passed in request-id"
-  [conn id]
-  (.cancelRealTimeBars conn id))
+  [{:keys [ecs]} id]
+  (.cancelRealTimeBars ecs id))
 
 (defn request-news-bulletins
   "Call this function to start receiving news bulletins. Each bulletin will
    be sent in a :news-bulletin, :exchange-unavailable, or :exchange-available
    message."
-  ([conn]
-   (request-news-bulletins conn true))
-  ([conn all-messages?]
-   (.reqNewsBulletins conn all-messages?)))
+  ([{:keys [ecs]}]
+   (request-news-bulletins ecs true))
+  ([{:keys [ecs]} all-messages?]
+   (.reqNewsBulletins ecs all-messages?)))
 
 (defn cancel-news-bulletins
   "Call this function to stop receiving news bulletins."
-  [conn]
-  (.cancelNewsBulletins conn))
+  [{:keys [ecs]}]
+  (.cancelNewsBulletins ecs))
 
 (defn request-fundamental-data
   "Call this function to receive Reuters global fundamental data. There must be a
    subscription to Reuters Fundamental set up in Account Management before you
    can receive this data."
-  ([conn contract report-type]
-   (request-fundamental-data conn (get-request-id) contract report-type))
-  ([conn request-id contract report-type]
-   (.reqFundamentalData conn request-id
+  ([{:keys [ecs]} contract report-type]
+   (request-fundamental-data ecs (get-request-id) contract report-type))
+  ([{:keys [ecs]} request-id contract report-type]
+   (.reqFundamentalData ecs request-id
                         (map-> com.ib.client.Contract contract)
                         (translate :to-ib :report-type report-type))))
 
 (defn cancel-fundamental-data
   "Call this function to stop receiving Reuters global fundamental data."
-  [conn request-id]
-  (.cancelFundamentalData conn request-id))
+  [{:keys [ecs]} request-id]
+  (.cancelFundamentalData ecs request-id))
 
 (defn request-contract-details
   "Call this function to download all details for a particular
   contract. The contract details will be received in a :contract-details
   message"
-  ([conn contract]
-   (request-contract-details conn (get-request-id) contract))
-  ([conn request-id contract]
+  ([{:keys [ecs]} contract]
+   (request-contract-details ecs (get-request-id) contract))
+  ([{:keys [ecs]} request-id contract]
    (log/debug "Requesting contract details #" request-id " for " (pr-str contract))
-   (.reqContractDetails conn request-id
+   (.reqContractDetails ecs request-id
                         (map-> com.ib.client.Contract contract))
    request-id))
 
 (defn place-order
-  ([conn contract order]
+  ([{:keys [ecs]} contract order]
    (let [order-id (get-order-id)]
-     (place-order conn order-id contract (assoc order :order-id order-id))))
-  ([conn order-id contract order]
-   (.placeOrder conn order-id
+     (place-order ecs order-id contract (assoc order :order-id order-id))))
+  ([{:keys [ecs]} order-id contract order]
+   (.placeOrder ecs order-id
                 (map-> com.ib.client.Contract contract)
                 (map-> com.ib.client.Order order))))
 
 (defn cancel-order
-  [conn order-id]
-  (.cancelOrder conn order-id))
+  [{:keys [ecs]} order-id]
+  (.cancelOrder ecs order-id))
 
-(defn request-open-orders [conn]
-  (.reqOpenOrders conn))
+(defn request-open-orders [{:keys [ecs]}]
+  (.reqOpenOrders ecs))
 
 (defn request-executions
-  ([conn]
-   (request-executions conn nil))
-  ([conn client-id]
-   (.reqExecutions conn client-id)))
+  ([{:keys [ecs]}]
+   (request-executions ecs nil))
+  ([{:keys [ecs]} client-id]
+   (.reqExecutions ecs client-id)))
 
 (defn request-account-updates
-  [conn subscribe? account-code]
-  (.reqAccountUpdates conn subscribe? account-code))
+  [{:keys [ecs]} subscribe? account-code]
+  (.reqAccountUpdates ecs subscribe? account-code))
 
 (defn set-server-log-level
   "Call this function to set the log level used on the server."
-  [conn log-level]
-  (.setServerLogLevel conn (translate :to-ib :log-level log-level)))
+  [{:keys [ecs]} log-level]
+  (.setServerLogLevel ecs (translate :to-ib :log-level log-level)))
 
-(defn connect
+(defn connect-eclientsocket
   "This function must be called before any other. There is no feedback
    for a successful connection, but a subsequent attempt to connect
    will return the message 'Already connected.'
@@ -573,9 +573,9 @@
 
    client-id identifies this client. Only one connection to a gateway can
    be made per client-id at a time."
-  ([ch] (connect ch "localhost" 7496))
-  ([ch client-id] (connect ch "localhost" 7496 client-id))
-  ([ch host port] (connect ch host port @client-id))
+  ([ch] (connect-eclientsocket ch "localhost" 7496))
+  ([ch client-id] (connect-eclientsocket ch "localhost" 7496 client-id))
+  ([ch host port] (connect-eclientsocket ch host port @client-id))
   ([ch host port client-id]
    (try
      (let [connection (com.ib.client.EClientSocket. (create-wrapper ch))]
@@ -586,18 +586,23 @@
      (catch Exception ex
        (log/error "Error trying to connect to " host ":" port ": " ex)))))
 
+(defn connect []
+  (let [ch (chan)]
+    {:ecs (connect-eclientsocket ch (swap! client-id inc))
+     :resp-chan ch}))
+
 (defn disconnect
   "Call this function to terminate the connections with TWS.
    Calling this function does not cancel orders that have already been sent."
-  [conn]
-  (.eDisconnect conn))
+  [{:keys [ecs]}]
+  (.eDisconnect ecs))
 
-(defn request-current-time [conn]
-  (.reqCurrentTime conn))
+(defn request-current-time [{:keys [ecs]}]
+  (.reqCurrentTime ecs))
 
-(defn connection-time [conn]
-  (->> (.TwsConnectionTime conn)
+(defn connection-time [{:keys [ecs]}]
+  (->> (.TwsConnectionTime ecs)
        (translate :from-ib :connection-time)))
 
-(defn request-server-version [conn]
-  (.serverVersion conn))
+(defn request-server-version [{:keys [ecs]}]
+  (.serverVersion ecs))

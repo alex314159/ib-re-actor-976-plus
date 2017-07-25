87a18bda6fc33265f207a263d857a7afc58cf306
Jean-Sebastien A. Beaudry <git@jsab.me>
2017-07-25 10:24:53 -0400

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject ib-re-actor "0.1.10"
+(defproject ib-re-actor "0.1.11-SNAPSHOT"
   :description "Clojure friendly wrapper for InteractiveBrokers java API"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.13.0"]
-                 [org.clojure/tools.logging "0.3.1"]
+                 [clj-time "0.14.2"]
+                 [org.clojure/tools.logging "0.4.0"]
                  [clj-logging-config "1.9.12"]]
   :profiles {:dev {:dependencies [[twsapi "9.71.01"]
-                                  [midje "1.8.3"]]
+                                  [midje "1.9.1"]]
                    :plugins [[lein-midje "3.2.1"]
-                             [lein-localrepo "0.5.3"]
                              [com.gfredericks/how-to-ns "0.1.6"]]
                    :how-to-ns {:require-docstring? false}}})

09a9191f0f4f6e9b4f81ddd9afc6d4073359e502
alex <alexandre.almosni@gmail.com>
2020-09-28 10:16:24 +0100

    separator for windows (ewrapper versioning)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ib-re-actor-976-plus "0.1.1-SNAPSHOT"
+(defproject ib-re-actor-976-plus "0.1.2-SNAPSHOT"
   :description "Clojure friendly wrapper for InteractiveBrokers Java API"
   :url "https://github.com/alex314159/ib-re-actor-976-plus"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
                  ;[clj-time "0.14.2"]
                  [org.clojure/tools.logging "1.0.0"]
                  [clj-logging-config "1.9.12"]]
-  :profiles {:dev {:dependencies [[twsapi "9.76.01"]
+  :profiles {:dev {:dependencies [[twsapi "9.80.03"]
                                   [midje "1.9.1"]]
                    :plugins [[lein-midje "3.2.1"]
                              [com.gfredericks/how-to-ns "0.1.6"]
